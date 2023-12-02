84847108bbfc7fad0ac5a9ad08934fdfb78cda82
Senat Balota <senattbalota@gmail.com>
2023-12-02 13:42:53 +0300

    projeyi inşa etmek için gerekli aşamalar yazıldı.

---
+++ project.js
@@ -0,0 +1,5 @@
+// 1. Para yatırma işlemi
+// 2. Hangi hat için bahis oluşturduğunu tespit etmek
+// 3. Bahis miktarını almak
+// 4. Slot makinesini çalıştırmak
+// 5. kullanıcının kazanıp kazanmadığını kontrol etmek
